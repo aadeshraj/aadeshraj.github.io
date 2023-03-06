2f7d857cb50f2a8aa3920aa9bbeddc3fd51e96c5
Aadesh Raj Sharma <aadeshrajsharma@gmail.com>
2023-03-06 22:13:41 +0545

    created landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { Box, Center, Divider, Stack, Text } from "@chakra-ui/react"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Center className="line" pos="absolute" backgroundColor="#0f0e0ea1" zIndex="1" width="100%" height="100vh"> 
+      <Stack direction='column' h='100px' p={4} spacing={2}>
+  <Text className="lineUp" fontSize='6xl' zIndex="1" color="white" align="center">Aadesh Raj Sharma</Text>
+  <Divider className="right" orientation='horizontal' />
+
+</Stack>
+      </Center>
+      
+      
+      <Box
+        backgroundImage="/bg.jpg"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        position="fixed"
+        height="100%"
+        width="100%"
+      />
+
+ 
+
+
     </div>
   );
 }
